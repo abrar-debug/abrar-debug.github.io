b47d16e7bcf4320b8a5ac66eaa34bcd196677220
abrar-debug <hoqueabrar@gmail.com>
2025-12-09 21:10:20 +0200

    Updates

--- _next/static/6xTwRji151hsw-wBXJ1dS/_buildManifest.js
+++ _next/static/6xTwRji151hsw-wBXJ1dS/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/abrar-portfolio//_next/:path+",
+        "destination": "/abrar-portfolio/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
